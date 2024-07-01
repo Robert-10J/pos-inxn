0eb16a355b115edc4a715335e5a0d15d142dc210
Robert-10J <roberttv17@gmail.com>
2024-07-01 13:18:44 -0600

    cambio de comillas dobles a sencillas

--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -1,5 +1,5 @@
-import Header from "../components/header/Header"
-import Sidebar from "../components/sidebar/Sidebar"
+import Header from '../components/header/Header'
+import Sidebar from '../components/sidebar/Sidebar'
 
 const Layout: React.FC = () => {
   return (
